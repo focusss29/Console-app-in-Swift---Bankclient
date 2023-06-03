654e74189b6935faf438e331674b31e5eff9e7f6
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-06-04 02:02:22 +0600

    Add protocol - Card and Account

---
+++ Bank/enum .swift
@@ -0,0 +1,14 @@
+enum StatusCard: String {
+    case available = "Доступна"
+    case blocked = "Заблокирована"
+}
+enum Currency: String {
+    case kzt = "₸"
+    case usd = "$"
+    case euro = "€"
+    case rus = "₽"
+}
+enum PaymentSystem: String {
+    case visa = "Visa"
+    case masterCard = "MasterCard"
+}
--- Bank/main.swift
+++ Bank/main.swift
@@ -1,2 +1,25 @@
 import Foundation
 
+protocol Account {
+    var paymentSystem: PaymentSystem { get }
+    var card: Card? { get set }
+    var status: StatusCard { get set }
+    var currency: Currency { get }
+    var balance: Double { get set }
+    
+    func checkBalance() -> String
+    func changePinCode(pinCode: Int)
+    func changeStatus(status: StatusCard)
+    func createCard(card: Card) -> [Card]
+    func closeCard(card: Card) -> [Card]
+    func transaction()
+}
+protocol Card {
+    var numberCard: String { get }
+    var date: Int { get }
+    var cvv: Int { get }
+    var holder: String { get }
+    var pinCode: Int { get set }
+}
+
+
